55f42c59af70e08cb1d4bd5fbd9a8417499a7a8b
meshackc0de <meshackjohn420@gmail.com>
2025-08-04 11:30:24 +0300

    quiz.js update

--- quiz.js
+++ quiz.js
@@ -4,12 +4,9 @@ const submitAnswer = document.getElementById('submit-answer');
 
 function checkAnswer(){
   let correctAnswer = '4';
-  //retrieve user's answer
   const userAnswer = document.querySelector('[name="quiz"]:checked').value;
-
-// Compare the User’s Answer with the Correct Answer
-
-  if (userAnswer === correctAnswer) {
+  if (userAnswer === correctAnswer)
+     {
     feedback.textContent = 'Correct! Well done.';
   } else {
     feedback.textContent = "That's incorrect. Try again!";

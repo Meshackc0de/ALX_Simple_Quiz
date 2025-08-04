f515702b36aa869a17602fbd57e6bc115f913634
meshackc0de <meshackjohn420@gmail.com>
2025-08-04 11:21:40 +0300

    add quiz.js

---
+++ quiz.js
@@ -0,0 +1,19 @@
+const feedback = document.getElementById('feedback');
+const submitAnswer = document.getElementById('submit-answer');
+
+
+function checkAnswer(){
+  let correctAnswer = '4';
+  //retrieve user's answer
+  const userAnswer = document.querySelector('[name="quiz"]:checked').value;
+
+// Compare the User’s Answer with the Correct Answer
+  if(userAnswer === correctAnswer){
+    feedback.textContent = 'Correct! Well done.';
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+
+
+submitAnswer.addEventListener('click', checkAnswer);
